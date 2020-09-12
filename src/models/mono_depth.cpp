8afdabad305b3fedeaddcd9dfff5cee8423bccfc
kemin.li <21636037@zju.edu.cn>
2020-09-12 11:13:31 +0800

    unify the mono interface

--- src/models/mono_depth.cpp
+++ src/models/mono_depth.cpp
@@ -1,4 +1,8 @@
 #include "mono_depth.h"
+#include <opencv2/core.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
 
 void MonoDepth::vis(
   cv::Mat& img,
@@ -7,15 +11,16 @@ void MonoDepth::vis(
   cv::Mat depth = outputs["depth"];
   cv::Mat img_color;
 
-  int height = inOutDims_["depth"].d[1];
-  int width  = inOutDims_["depth"].d[2];
+  int height = inOutDims_["depth"].d[2];
+  int width  = inOutDims_["depth"].d[3];
 
   img_color.create(height, width, CV_8UC1);
 
   for (int y = 0; y < height; y ++)
     for (int x = 0; x < width; x ++)
     {
-      img_color.at<uchar>(y,x) = static_cast<uchar>(std::min(depth.at<float>(0,y,x)/maxDepth_*255, 255.0f));
+      int index[4] = {0,0,y,x};
+      img_color.at<uchar>(y,x) = static_cast<uchar>(std::min(depth.at<float>(index)/maxDepth_*255, 255.0f));
     }
 
   cv::applyColorMap(img_color, img_color, cv::COLORMAP_HOT);
